b9098ddf8fd74d6b49b92c8160f06965e7f0eb37
sevvalozdemir <115032656+sevvalozdemir@users.noreply.github.com>
2022-11-09 17:38:05 +0300

    Add files via upload

---
+++ uc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+
+int main() {
+	int s,k;
+	int yildiz=1;
+	for(k=0;k<6;k++){
+	
+	for(s=0;s<yildiz;s++)
+	{
+		printf("*");
+	}
+	printf("\n");
+	yildiz++;
+}
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
