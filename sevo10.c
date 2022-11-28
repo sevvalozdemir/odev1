d611366e01148cfda9f5b51e56478ba3900f21c1
sevvalozdemir <115032656+sevvalozdemir@users.noreply.github.com>
2022-11-29 01:21:33 +0300

    Add files via upload

---
+++ sevo10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int usAlma(int, int);
+ 
+int main() {
+    int taban = 5;
+    int us = 2;
+    int sonuc;
+ 
+    sonuc = usAlma(taban, us);
+ 
+    printf("(%d)^%d = %d\n", taban, us, sonuc);
+    return 0;
+}
+ 
+int usAlma(int taban, int us) {
+    if (us == 0)
+        return 1;
+    return taban * usAlma(taban, us - 1);
+}
