ff6081a1120f06ac1f7dc5699ac8ca414c5c3101
sevvalozdemir <115032656+sevvalozdemir@users.noreply.github.com>
2022-11-29 10:14:02 +0300

    Add files via upload

---
+++ sevoo15.c
@@ -0,0 +1,11 @@
+double sayi(double s1, double s2);
+int main()
+{
+    double sonuc = sayi(12.3, 6.4);
+    printf("%lf", sonuc);
+    return 0;
+}
+double sayi(double s1, double s2)
+{
+    return s1 + s2;
+}
